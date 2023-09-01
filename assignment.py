877d05f6f6fa81afc555efffd3e8e41a434ffdee
MUKESH RAJ <rajmukeshjii2000@gmail.com>
2023-09-01 21:34:41 +0530

    initial

---
+++ assignment.py
@@ -0,0 +1,17 @@
+
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+# 1. Floor Division
+floor_division_result = a // b
+
+# 2. a power b
+power_result = a ** b
+
+# 3. Check if a and b have equal values
+are_equal = a == b
+
+# Print the results
+print("1. Floor Division:", floor_division_result)
+print("2. a power b:", power_result)
+print("3. Are a and b equal:", are_equal)
